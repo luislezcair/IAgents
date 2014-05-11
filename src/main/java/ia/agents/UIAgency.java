c8efb2323e29f443c672c351c2abbeba0c9b9eed
Luis Lezcano Airaldi <luislezcair@gmail.com>
2014-05-11 20:51:22 -0300

    Interfaces para los agentes y una principal. Agrego documentación de JADE para tener autocompletado con javadoc

---
+++ src/main/java/ia/agents/UIAgency.java
@@ -0,0 +1,12 @@
+package ia.agents;
+
+import javax.swing.*;
+
+/**
+ * Created by Luis Lezcano Airaldi.
+ * Part of IAgents
+ */
+public class UIAgency {
+    private JButton button1;
+    private JPanel panel1;
+}
---
+++ src/main/java/ia/main/AgentManager.java
@@ -0,0 +1,8 @@
+package ia.main;
+
+/**
+ * Created by Luis Lezcano Airaldi.
+ * Part of IAgents
+ */
+public class AgentManager {
+}
