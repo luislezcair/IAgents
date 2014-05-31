4cce6c2595e3bcfc276b63ae2809fd0d024cee42
Luis Lezcano Airaldi <luislezcair@gmail.com>
2014-05-31 01:26:28 -0300

    Interfaz principal para ejecutar agentes.

--- src/main/java/ia/main/AgentManager.java
+++ src/main/java/ia/main/AgentManager.java
@@ -1,35 +1,62 @@
+/**
+ * Created by IA - Grupo 3.
+ * Part of IAgents
+ */
+
 package ia.main;
 
+import ia.main.ui.UIAgentManager;
 import jade.core.*;
 import jade.core.Runtime;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
 import jade.wrapper.StaleProxyException;
 
-/**
- * Created by IA - Grupo 3.
- * Part of IAgents
- */
 public class AgentManager {
+    Runtime rt;
     AgentContainer mainContainer;
+    AgentController rma;
+    UIAgentManager ui;
+
+    public AgentManager() {
+        ui = new UIAgentManager(this);
+        ui.show();
+
+        rt = Runtime.instance();
+    }
 
     public void launchRuntime() {
         Profile profile = new ProfileImpl();
         profile.setParameter(Profile.PLATFORM_ID, "IAMainPlatform");
         profile.setParameter(Profile.CONTAINER_NAME, "IAMainContainer");
 
-        Runtime rt = Runtime.instance();
         mainContainer = rt.createMainContainer(profile);
     }
 
     public void launchRma() {
         try {
-            AgentController rma =
-                    mainContainer.createNewAgent(
-                            "rma", "jade.tools.rma.rma", null);
+            rma = mainContainer.createNewAgent(
+                    "rma", "jade.tools.rma.rma", null);
             rma.start();
         } catch(StaleProxyException e) {
             e.printStackTrace();
         }
+    }
+
+    public void launchTestAgents() {
+        
+    }
+
+    public void shutdown() {
+        if(rma != null) {
+            try {
+                rma.kill();
+                mainContainer.kill();
+            } catch (StaleProxyException e) {
+                e.printStackTrace();
+            }
+        }
+        ui.dispose();
+        rt.setCloseVM(true);
     }
 }
--- src/main/java/ia/main/ui/UIAgentManager.java
+++ src/main/java/ia/main/ui/UIAgentManager.java
@@ -1,8 +1,37 @@
-package ia.main.ui;
-
 /**
  * Created by IA - Grupo 3.
  * Part of IAgents
  */
+
+package ia.main.ui;
+
+import ia.main.AgentManager;
+
+import javax.swing.*;
+
 public class UIAgentManager {
+    private AgentManager manager;
+    private JFrame mainWindow;
+    private JPanel mainPanel;
+    private JButton buttonSalir;
+    private JButton buttonTestAgents;
+
+    public UIAgentManager(AgentManager am) {
+        manager = am;
+
+        mainWindow = new JFrame("Sistema Multiagente de Turismo");
+        mainWindow.getContentPane().add(mainPanel);
+        mainWindow.pack();
+
+        buttonSalir.addActionListener(event -> manager.shutdown());
+        buttonTestAgents.addActionListener(event -> manager.launchTestAgents());
+    }
+
+    public void show() {
+        mainWindow.setVisible(true);
+    }
+
+    public void dispose() {
+        mainWindow.dispose();
+    }
 }
